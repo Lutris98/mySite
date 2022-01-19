139372d7c6eaa0199a3416bd58ffa880d329d590
Lutris98 <johnyou98@yonsei.ac.kr>
2022-01-19 20:40:57 +0900

    Thanks Lord

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<h1>장고 앱 만들기</h1>
 cmd창에</br>
 django-admin startproject mySite</br>
 python manage.py startapp Overview</br>
@@ -8,3 +9,9 @@ python manage.py startapp Overview</br>
 4. mySite/urls.py에 연결</br>
 
 참고로 css,js등은 Overview/static/css로 따로 넣어야함</br>
+
+<h1>MySQL과 연동</h1>
+1. mysql_settings.py 만들고 .gitignore에 포함</br>
+2. python manage.py inspectdb</br>
+3. 복붙해 쓸 앱의 models.py에 넣음</br>
+4. view.py와 urls.py에 반영</br>
--- mySite/settings.py
+++ mySite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sxya!s28ajr90@(_sldd_tqj7*xj&2z_7b50svfrr2@enm=*q3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Overview',
+    'Home',
 ]
 
 MIDDLEWARE = [
@@ -71,15 +69,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mySite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import mysql_settings  # 장고 key, mysql key따로 관리해 gitignore
+DATABASES = mysql_settings.DATABASES
+SECRET_KEY = mysql_settings.SECRET_KEY
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
